ef12f2e318529220e35d69013db4710d361f7775
Apex-Incarna <emenser05@gmail.com>
2022-08-30 21:55:55 -0400

    (hopefully) Final commit for Assignment Six

--- questionsJS.js
+++ questionsJS.js
@@ -1,5 +1,7 @@
-
+// I had to create a separate JS file because although the original would link to my questions page, none of the javaScript would actually work...thoughts? 
 // These elements set up the Question submission box
+// I consider this to be my part two, part three, and part four, as it includes elements from all three (button, input, and the user interaction of adding questions), although I'll admit it doesn't satisfy 100% the requirements of each indivdual part
+// Please tell me if you think this is sufficient for Assignment Six
 postButton = document.getElementById("postButton");
 input = document.getElementById("inputQuestion");
 questionsText = document.getElementById("questionsText");
